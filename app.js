8f4eef263126d1c213af74f0b0e0bebc625ecd7e
AyomiCoder <alukostephen01@gmail.com>
2024-09-05 11:07:50 +0100

    favicon

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const helmet = require('helmet'); // Import helmet
+
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,16 +11,6 @@ app.use(bodyParser.json()); // Parse JSON instead of urlencoded
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Set Content Security Policy (CSP) to allow 'data:' URLs for images
-app.use(
-  helmet.contentSecurityPolicy({
-    useDefaults: true,
-    directives: {
-      defaultSrc: ["'self'"],
-      imgSrc: ["'self'", "data:"], // Allow 'data:' URLs for images
-    },
-  })
-);
 
 // Routes
 const indexRouter = require('./routes/index');
