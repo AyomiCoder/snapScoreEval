e8e1811596867c3bd1994019c152e1741d12d492
AyomiCoder <alukostephen01@gmail.com>
2024-09-05 11:12:50 +0100

    views

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.json()); // Parse JSON instead of urlencoded
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
