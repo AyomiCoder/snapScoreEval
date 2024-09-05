b9850cdb700d2bf78c1c42ab109c21cada016bcf
AyomiCoder <alukostephen01@gmail.com>
2024-09-05 10:57:46 +0100

    urlimg

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const helmet = require('helmet'); // Import helmet
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +10,17 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json()); // Parse JSON instead of urlencoded
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Set Content Security Policy (CSP) to allow 'data:' URLs for images
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      defaultSrc: ["'self'"],
+      imgSrc: ["'self'", "data:"], // Allow 'data:' URLs for images
+    },
+  })
+);
 
 // Routes
 const indexRouter = require('./routes/index');
